18a81edb2311cc84cf315e467a31d06e9250b28d
Nicole Han <nicolehan04@gmail.com>
2024-04-22 16:42:42 -0400

    add memoization

--- webcrawler.py
+++ webcrawler.py
@@ -1,6 +1,26 @@
 from twikit import Client, build_query
 from langdetect import detect
 import configparser
+import pickle
+
+# save memoized data
+memoized_searches = {}
+
+# retrieve memoized searches
+def load_memoized_searches():
+    try:
+        with open("memoized_searches.pkl", "rb") as file:
+            return pickle.load(file)
+    except FileNotFoundError:
+        return {}
+    
+# save memoized searches
+def save_memoized_searches():
+    with open("memoized_searches.pkl", "wb") as file:
+        pickle.dump(memoized_searches, file)
+        
+# load initially saved data
+memoized_searches = load_memoized_searches()
 
 # parse account data
 config = configparser.ConfigParser()
@@ -30,6 +50,12 @@ def filter_english(tweets):
     
 # function search tweets 
 def search_tweets(text, options):
+    # check if the result is already memoized
+    cache_key = (text, options.get('date'))  
+    if cache_key in memoized_searches:
+        print("memoized")
+        return memoized_searches[cache_key]
+    
     # build query
     query = build_query(text=text, options=options)
     
@@ -42,4 +68,13 @@ def search_tweets(text, options):
     for tweet in tweets:
         results.append(tweet.text)
         
-    return filter_english(results)
+    filtered_results = filter_english(results)
+    
+    # memoize results
+    memoized_searches[cache_key] = filtered_results
+    
+    # save memoized results to file
+    save_memoized_searches()
+    
+    # return results
+    return filtered_results
